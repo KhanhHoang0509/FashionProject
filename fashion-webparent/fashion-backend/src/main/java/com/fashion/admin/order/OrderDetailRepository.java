b420a61435ec35ac55b74707e01e30953b49ab6f
KhanhHoang0509 <89851793+KhanhHoang0509@users.noreply.github.com>
2023-04-30 15:12:33 +0700

    Update OrderDetailRepository.java

--- fashion-webparent/fashion-backend/src/main/java/com/fashion/admin/order/OrderDetailRepository.java
+++ fashion-webparent/fashion-backend/src/main/java/com/fashion/admin/order/OrderDetailRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 
 public interface OrderDetailRepository extends CrudRepository<OrderDetail, Integer> {
 	
-	@Query("SELECT NEW com.shoppingcart.common.entity.order.OrderDetail(d.product.category.name, d.quantity,"
+	@Query("SELECT NEW com.fashion.fashioncommon.entity.order.OrderDetail(d.product.category.name, d.quantity,"
 			+ " d.productCost, d.shippingCost, d.subtotal)"
 			+ " FROM OrderDetail d WHERE d.order.orderTime BETWEEN ?1 AND ?2")
 	public List<OrderDetail> findWithCategoryAndTimeBetween(Date startTime, Date endTime);
 	
-	@Query("SELECT NEW com.shoppingcart.common.entity.order.OrderDetail(d.quantity, d.product.name,"
+	@Query("SELECT NEW com.fashion.fashioncommon.entity.order.OrderDetail(d.quantity, d.product.name,"
 			+ " d.productCost, d.shippingCost, d.subtotal)"
 			+ " FROM OrderDetail d WHERE d.order.orderTime BETWEEN ?1 AND ?2")
 	public List<OrderDetail> findWithProductAndTimeBetween(Date startTime, Date endTime);	
